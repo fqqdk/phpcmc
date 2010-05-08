6fea02c142aa5fb29ddd55465fc1348f533b2341
fqqdk <fqqdk@clusterone.hu>
2010-05-08 16:27:40 +0200

    added default ignores

---
+++ tests/_conf/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @author fqqdk <fqqdk@freemail.hu>
+ */
+
+/**
+ * Bootstrap function that encapsulates bootstrap behaviour
+ *
+ * @return void
+ */
+function __bootstrap()
+{
+}
+
+__bootstrap();
+
+?>
